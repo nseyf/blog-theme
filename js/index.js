8081757ce6579b5eb1213af6518a388e2a496b0e
Nima Seyf <nimaseyf@gmail.com>
2017-01-31 13:46:34 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var scrollFunction = function scrollFunction() {
+  var scrollPosition = document.body.scrollTop;
+  var frontPageHeight = document.getElementById('#splash').style.height;
+  if (scrollPosition > frontPageHeight) {}
+};
+
+var elem = document.getElementById('#splash').style.height;
